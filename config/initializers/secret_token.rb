051ff2dd159a108c0e036776f85c6cb16c94ccf2
Shaine Hatch <hatch@fadelight.com>
2011-07-21 18:56:51 -0600

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contest::Application.config.secret_token = '24fb8e7e03a20dcb43d0ce9bd8f2bc93b142b787d5d4cdfa20c620f516fb45446643f63ca38ee959a2f2318f01b40fc79d47855cb2fae877e74a5878bdad2945'
