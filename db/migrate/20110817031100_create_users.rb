3083699edb9234e2c65fd5ee3ddc5448ad161366
Shaine Hatch <hatch@fadelight.com>
2011-08-16 21:14:07 -0600

    Users table

---
+++ db/migrate/20110817031100_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :username, :null => false
+      t.string :password, :null => false
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+      t.string :url
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
