8deeb6b0140a9e13271fbf6d06ad908a9e24b2e3
Shaine Hatch <hatch@fadelight.com>
2011-08-18 21:27:14 -0600

    Real users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Contest::Application.routes.draw do
+  get "users/new"
+
+  get "users/create"
+
+  get "users/update"
+
+  get "users/edit"
+
+  get "users/destroy"
+
+  get "users/index"
+
+  get "users/show"
+
   resources :users
 
   get "public/home"
