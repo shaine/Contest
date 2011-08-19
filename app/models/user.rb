11e0b19df6d61b5f4fc650c8d7f4cce65c69a692
Shaine Hatch <hatch@fadelight.com>
2011-08-18 20:45:47 -0600

    Updated user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  validates :url, :presence => true
 end
---
+++ db/migrate/20110819020133_additional_user_fields.rb
@@ -0,0 +1,17 @@
+class AdditionalUserFields < ActiveRecord::Migration
+  def self.up
+    change_table :users do |t|
+      t.string :gender, :null => false
+      t.string :agent
+      t.string :high_school
+      t.string :manager
+      t.string :home_town
+    end
+  end
+
+  def self.down
+    change_table :users do |t|
+      t.remove :gender, :agent, :high_school, :manager, :home_town
+    end
+  end
+end
