c17f62df89eae4b040039ce66ae00c54e1834f1a
Shaine Hatch <hatch@fadelight.com>
2011-10-21 19:24:43 -0600

    Basic user functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  require "digest"
+  
   validates :username, :presence => true,
                        :uniqueness => true,
                        :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
@@ -15,4 +17,17 @@ class User < ActiveRecord::Base
   def self.human_attribute_name(attribute, options = {})
     HUMAN_ATTRIBUTES[attribute] || super  
   end
+  
+  def authenticate(user, password)
+    password = Digest::MD5.hexdigest(password)
+    
+    User.where(:username => user, :password => password).first
+  end
+  
+  before_create :md5_password
+
+  private
+    def md5_password
+      self.password = Digest::MD5.hexdigest(self.password)
+    end
 end
