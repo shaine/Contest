4836546f90ec91e91b7a3992f94ac942cc921ee9
Shaine Hatch <hatch@fadelight.com>
2011-11-01 19:12:53 -0600

    Seeeeeds!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,5 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-users = User.create([{:username => 'test@test.com',
-                      :password => Digest::MD5.hexdigest('test'),
-                      :first_name => 'Shaine',
-                      :last_name => 'Hatch',
-                      :url => 'shatch',
-                      :gender => 'male',
-                      :agent => 'Jon Greene',
-                      :manager => 'Jason Skollingsberg',
-                      :high_school => 'Cyprus High',
-                      :home_town => 'Magna, UT'
-                    }])
+user = User.create(:username => 'test@test.com', :password => 'test', :password_confirmation => 'test', :first_name => 'Shaine', :last_name => 'Hatch', :url => 'shatch', :gender => 'male', :agent => 'Jon Greene', :manager => 'Jason Skollingsberg', :high_school => 'Cyprus High', :home_town => 'Magna, UT')
+
