33eb8b465b661aa59e90da1261c77d09b8c6d463
Shaine Hatch <hatch@fadelight.com>
2011-11-03 10:11:55 -0600

    Cukecd www/contest/

---
+++ features/step_definitions/users.rb
@@ -0,0 +1,20 @@
+Given /^I am logged out$/ do
+  click_link("Logout")
+end
+
+When /^I attempt to login:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  hash = table.hashes.first
+  fill_in('login_user[username]', :with => hash[:username])
+  fill_in('login_user[password]', :with => hash[:password])
+  uncheck('stay_logged_in')
+  check('stay_logged_in') if hash[:stay_logged_in]
+end
+
+Then /^I should be logged in$/ do
+  assert has_content? "Logout"
+end
+
+Then /^I should see the private template$/ do
+  assert has_content? "Dashboard"
+end
